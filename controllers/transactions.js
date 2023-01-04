321411891296322df2c6d70129db95e7efe575f4
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-04 17:12:17 -0500

    removed fly deploy files

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,11 +3,6 @@ const router = express.Router()
 const { v4: uuidv4 } = require('uuid')
 const data = require("../models/data.js")
 const {validateObj} = require("../models/validations.js")
-router.use((req, resp, next) =>{
-    console.log(uuidv4())
-    req.body.id = uuidv4()
-    next()
-})
 
 // get data route
 router.get("/", (req, resp) => {
@@ -17,12 +12,15 @@ router.get("/", (req, resp) => {
 // POST (create) route
 router.post("/", validateObj, (req, resp) => {
     const idValue = uuidv4()
-
     data.push({["id"]:idValue, ...req.body})
     resp.status(200).json(data.at(-1))
 })
 
-// 
+// SHOW route (search for id key value)
+router.get("/:id", (req, resp) => {
+    
+})
+
 
 
 
