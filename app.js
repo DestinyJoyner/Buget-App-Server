f7d605049b0caddd38d98ccbe35f3ceed7391be7
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-09 13:05:47 -0500

    added app use cors middleware access control

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+})
 app.use("/transactions", require("./controllers/transactions.js"))
 
 
